d7ff37fa9d42b7ee6dd3552000feb7a11ca2c910
hassan <ui24@fardup.co>
2025-08-06 14:34:31 +0330

    ✨ تغییر پوشه، بیلد و سینک اندروید

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.example.app;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
 
-public class MainActivity extends BridgeActivity {}
+import java.util.ArrayList;
+
+import com.capacitorjs.plugins.filetransfer.FileTransferPlugin; // ⬅️ این خط الزامی است
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      add(FileTransferPlugin.class); // ⬅️ این خط الزامی است
+    }});
+  }
+}
